592b029d877a25f3fe3437f8d4588ba0398ba8c7
Pradeep Kolli <pradeepkolli3@yahoo.in>
2019-09-22 05:48:50 -0500

    added contacts and stuff

--- FeastVite/viewControllers/ChatViewController.swift
+++ FeastVite/viewControllers/ChatViewController.swift
@@ -8,15 +8,34 @@
 
 import UIKit
 
-class ChatViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class ChatViewController: UIViewController, UITextViewDelegate, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var sendMessageBTN: UIButton!
+    @IBOutlet weak var messageTextViewTV: UITextView!
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myTextCell", for: indexPath)
+//        let cell2 = tableView.dequeueReusableCell(withIdentifier: "contactsTextCell", for: indexPath)
+        return cell
     }
     
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.delegate = self
+    self.messageTextViewTV.layer.borderWidth = 2
+    self.messageTextViewTV.layer.cornerRadius = 15
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func sendActionBTN(_ sender: Any) {
+    }
+    
     /*
     // MARK: - Navigation
 
